8480a64398d8657edf76a2878b2ea06c8a71c431
shashank <shankyz93@gmail.com>
2022-12-18 06:36:23 +0530

    Hello world java file

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+/* HelloWorld.java
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("")
+		System.out.println("This is a sample program")
+		System.out.println("Hello World!");
+            System.out.println("")
+		System.out.println("")
+
+	}
+}
